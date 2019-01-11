72afa066d36b149e10532b3849dd91ea9e712be1
Carlo Operio <operiocarlo@yahoo.com>
2019-01-10 23:37:50 -0500

    v.1.0-alpha - Menubar now functions like a standard menubar
    CHANGE
    -Remade Menumaster to accomodate new features like opened submenu sliding, toggling on and off
    -Changed the name in menubar.ini to Slim-menubar
    
    REFACTOR
    -Placed constants in their own variables for easier editing
    
    REMOVE
    -Removed empty directories in the top menubar in menu.json

--- scripts/MenuMaster.lua
+++ scripts/MenuMaster.lua
@@ -1,123 +1,176 @@
-loadedMenu = "menu not acquired";
+local USER_CREATED_MENU_JSON_PATH = "data/menu.json"
+local MENUS_PATH = "sections/menus.inc"
+local JSON_SCRIPT_PATH = "scripts/json.lua"
 
-local json = {}
-local userCreatedMenusPath = "data/menu.json"
-local menusPath = "sections/menus.inc"
-local jsonScriptPath = "scripts/json.lua"
+local COMMENTS = "; DO NOT MANUALLY EDIT\n"
+	.. "; ALL EDITS WILL BE LOST ONCE IT DETECTS A NEW BUTTON\n"
+	.. "; MenuMaster.lua handles the editing of this file\n" 
 
-local base =   "; DO NOT MANUALLY EDIT\n"
-			.. "; ALL EDITS WILL BE LOST ONCE IT DETECTS A NEW BUTTON\n"
-			.. "; MenuMaster.lua handles the editing of this file\n" 
-			.. "[AddCustomVariables]\n"
-			.. "@includeVariables=\"../data/variables.inc\"\n"
-			.. "\n[MenuMaster]\n"
-			.. "Measure=Script\n"
-			.. "ScriptFile=scripts/MenuMaster.lua\n"
-			.. "Disabled=1\n"
+local ADD_CUSTOM_VARIABLES = "\n[AddCustomVariables]\n"
+	.. "@includeVariables=\"../data/variables.inc\"\n"
 
-function Initialize ()
-	json = assert(loadfile(SKIN:MakePathAbsolute(jsonScriptPath)))() 
-	UpdateMenusMeter()
-end
+local MENU_MASTER_MEASURE = "\n[MenuMaster]\n"
+	.. "Measure=Script\n"
+	.. "ScriptFile=scripts/MenuMaster.lua\n"
+	.. "Disabled=1\n"
+			
+local GENERIC_MENU_BUTTON_STYLE = "\n[GenericMenuButtonStyle]\n"
+	.. "FontColor=200,200,200,255\n"
+	.. "FontSize= 11\n"
+	.. "FontWeight=400\n"
+	.. "FontFace=Roboto\n"
+	.. "AntiAlias=1\n"
+	.. "StringAlign=LeftCenter\n"		
+	.. "ClipString=1\n"
 
-CurrentX = 0
-local groups = {}
-function SetCurrentX (meterName)
-	local meter = SKIN:GetMeter(meterName)
-	CurrentX = meter:GetX()
-end
+local GENERIC_SUBMENU_BUTTON_STYLE = "\n[GenericSubmenuButtonStyle]\n"
+	.. "SolidColor=#BackgroundColor#\n"
+	.. "Padding=10,3,0,3\n"
+	.. "DynamicVariables=1\n"
+	.. "W=250\n"
+	.. "H=25\n"
+	.. "Hidden=1\n"
 
-function UpdateMenusMeter () 
-
-	-- decode the menu.json
-	local data = ReadFile(userCreatedMenusPath)
-	data = json.decode(data)
-	if not data then return end	
-
-	-- Initialize
-	for key, value in pairs(data) do
-		table.insert(groups, value["name"])
-	end
-	local parameters = "\n[Rainmeter]\n"
-	-- .. "LeftMouseDownAction=[!CommandMeasure MenuMaster \"testprint()\"]"
-	.. "LeftMouseDownAction="
-	for key, value in pairs(groups) do 
-		parameters = parameters .. "["
-		parameters = parameters .. "!HideMeterGroup " .. value
-		parameters = parameters .. "]"
-	end
-	base = base .. parameters .. "[!Redraw]\n"
-
-	base = base .. "\n[PaddingLeft]\n"
+-- Invisibile meter that moves the margin of the menu to the right
+local PADDING_LEFT = "\n[PaddingLeft]\n"
 	.."Meter=String\n"
 	.."X=#PaddingLeft#\n"
 	.."Y=(#PaddingTop#+4)\n"
 	.."W=25\n"
 	.."H=25\n" 
+
+local RAINMETER_METER = "\n[Rainmeter]\n" 
+	.. "LeftMouseUpAction=[!CommandMeasure MenuMaster \"RainmeterOnClick()\"]\n"
+
+local groups = {}
+		
+function Initialize ()
 	
+	-- get json script
+	local json = assert(loadfile(SKIN:MakePathAbsolute(JSON_SCRIPT_PATH)))() 
+
+	-- get and decode the menu.json
+	local data = ReadFile(USER_CREATED_MENU_JSON_PATH)
+	data = json.decode(data)
+	if not data then return end	
+
+	-- Initialize Groups
+	for key, value in pairs(data) do
+		table.insert(groups, value["name"])
+	end
+
+	-- Combine the strings
+	menusInc = COMMENTS
+	.. RAINMETER_METER
+	.. ADD_CUSTOM_VARIABLES
+	.. MENU_MASTER_MEASURE
+	.. GENERIC_MENU_BUTTON_STYLE
+	.. GENERIC_SUBMENU_BUTTON_STYLE
+	.. PADDING_LEFT
+	.. CreateMenu(data)
+	
+	-- Write to menus.inc file
 	-- OPEN FILE.
-	local file = io.open(SKIN:MakePathAbsolute(menusPath), "w+")
+	local file = io.open(SKIN:MakePathAbsolute(MENUS_PATH), "w+")
 
 	-- HANDLE ERROR OPENING FILE.
 	if not file then
-		print('ReadFile: unable to open file at ' .. menusPath)
+		print('ReadFile: unable to open menus path at ' .. MENUS_PATH)
 		return
 	end
 
-	-- build the menu
-	local newMenuMeters = base .. CreateTopMenu(data)
 	io.output(file)
-	io.write(newMenuMeters)
+	io.write(menusInc)
 	io.close(file)
 end
 
-function testprint()
-	print(SKIN:GetMeter("Development"):GetOption("SolidColor", ""))
+local previousOpenedSubmenu = "None"
+local OpenedSubmenu = "None"
+
+CurrentX = 0
+
+function TopMenuOnClick (meterName)
+	local meter = SKIN:GetMeter(meterName)
+	CurrentX = meter:GetX()
+
+	-- close all submenus
+	for key,value in ipairs(groups) do
+		SKIN:Bang("!HideMeterGroup", value)
+	end
+
+	previousOpenedSubmenu = OpenedSubmenu
+	OpenedSubmenu = meterName
+
+	if(previousOpenedSubmenu == OpenedSubmenu) then
+
+		previousOpenedSubmenu = OpenedSubmenu
+		OpenedSubmenu = "None"
+	else 
+		SKIN:Bang("!ShowMeterGroup", OpenedSubmenu)
+	end
 end
 
-function CreateTopMenu (data) 
+function TopMenuOnMouseOver (meterName) 
+
+	if(OpenedSubmenu == "None") then return end
+
+	-- close all submenus
+	for key,value in ipairs(groups) do
+		SKIN:Bang("!HideMeterGroup", value)
+	end	
+	
+	local meter = SKIN:GetMeter(meterName)
+	CurrentX = meter:GetX()
+
+	OpenedSubmenu = meterName
+
+	SKIN:Bang("!ShowMeterGroup", OpenedSubmenu)
+	SKIN:Bang("!UpdateGroup", OpenedSubmenu)
+end
+
+function RainmeterOnClick () 
+
+	-- close all submenus
+	for key,value in ipairs(groups) do
+		SKIN:Bang("!HideMeterGroup", value)
+	end
+
+	previousOpenedSubmenu = OpenedSubmenu
+	OpenedSubmenu = "None"
+
+	-- SKIN:Bang("!Update")
+	SKIN:Bang("!Redraw")
+end
+
+function CreateMenu (data) 
 	local menuMeters = ""
 	local submenuMeters = ""
 
 	for key, value in pairs(data) do 
 		local name = value["name"]:gsub("%s", "")
-		local textName = value["name"]
 
 		menuMeters = menuMeters .. "\n["..name.."]\n"
 		.. "Meter=String\n"
-		.. "FontColor=200,200,200,255\n"
-		.. "FontSize= 11\n"
-		.. "FontWeight=400\n"
-		.. "FontFace=Roboto\n"
-		.. "AntiAlias=1\n"
-		.. "StringAlign=LeftCenter\n"
+		.. "MeterStyle = GenericMenuButtonStyle\n"
 		.. "SolidColor=0,0,0,1\n"
 		.. "Padding=10,3,0,3\n"
-		.. "ClipString=1\n"
-		.. "Text=" .. name .. "\n"
-		.. "MouseOverAction=[!SetOption "..name.." FontColor 255,255,255,255][!Update]\n"
+		.. "Text=" .. value["name"] .. "\n"
+		.. "MouseOverAction="
+			.. "[!SetOption "..name.." FontColor 255,255,255,255]"
+			.. "[!CommandMeasure MenuMaster \"TopMenuOnMouseOver(\'"..name.."\')\"]"
+			.. "[!Update]\n"
 		.. "MouseLeaveAction=[!SetOption "..name.." FontColor 200,200,200,255][!Update]\n"
 		.. "LeftMouseUpAction="
-		.. "[!ToggleMeterGroup "..textName.."]"
-		.. "[!CommandMeasure MenuMaster \"SetCurrentX(\'"..name.."\')\"][!Update][!Redraw]\n"
+			.. "[!ToggleMeterGroup "..name.."]"
+			.. "[!CommandMeasure MenuMaster \"TopMenuOnClick(\'"..name.."\')\"][!Update][!Redraw]\n"
 		.. "X=0R\n"
 		.. "Y=13\n"
 
+		-- Creates the submenu for the current top menu
 		submenuMeters = submenuMeters .. CreateSubMenu(value)
 	end
 
 	return menuMeters .. submenuMeters
-end
-
-function CloseOtherGroups () 
-	local parameters = ""
-	for key, value in pairs(groups) do 
-		parameters = parameters .. "["
-		parameters = parameters .. "!HideMeterGroup " .. value
-		parameters = parameters .. "]"
-	end
-	parameters = parameters .. "[!Update][!Redraw]\n"
-	return parameters
 end
 
 function CreateSubMenu (value)
@@ -128,47 +181,34 @@ function CreateSubMenu (value)
 
 	for key, value in pairs(submenus) do 
 		local name = value["name"]:gsub("%s", "")
-		local textName = value["name"]
 		local path = value["path"] and value["path"] or ""
 
 		menuMeters = menuMeters .. "\n["..name.."]\n"
 		.. "Meter=String\n"
-		.. "FontColor=200,200,200,255\n"
-		.. "FontSize= 11\n"
-		.. "FontWeight=400\n"
-		.. "FontFace=Roboto\n"
-		.. "AntiAlias=1\n"
-		.. "StringAlign=LeftCenter\n"
-		.. "SolidColor=#BackgroundColor#\n"
-		.. "Padding=10,3,0,3\n"
-		.. "ClipString=1\n"
-		.. "Text=" .. textName .. "\n"
+		.. "MeterStyle = GenericMenuButtonStyle | GenericSubmenuButtonStyle\n"
+		.. "Text=" .. value["name"] .. "\n"
 		.. "MouseOverAction=[!SetOption "..name.." FontColor 255,255,255,255][!Update]\n"
 		.. "MouseLeaveAction=[!SetOption "..name.." FontColor 200,200,200,255][!Update]\n"
-		.. "LeftMouseDownAction=["..path.."]"..CloseOtherGroups()
+		.. "LeftMouseDownAction=["..path.."]\n"
 		.. "Group="..topName.."\n"
-		.. "DynamicVariables=1\n"
-		.. "W=250\n"
-		.. "H=25\n"
 		.. "Y="..(key == 1 and "42" or "0R").."\n"
 		.. "X=[&MenuMaster:CurrentX]\n"
-		.. "Hidden=1\n"
 	end
 	return menuMeters
 end
 
--- unused
-function Traverse(table, fn) 
-	for key, value in pairs(table) do 
-		for tableKey, tableValue in pairs(value) do
-			if(tableKey == "submenu") then
-				Traverse(tableValue, fn)
-			else 
-				fn(tableKey, tableValue)
-			end
-		end
-	end
-end
+-- unused // kept for reference
+-- function Traverse(table, fn) 
+-- 	for key, value in pairs(table) do 
+-- 		for tableKey, tableValue in pairs(value) do
+-- 			if(tableKey == "submenu") then
+-- 				Traverse(tableValue, fn)
+-- 			else 
+-- 				fn(tableKey, tableValue)
+-- 			end
+-- 		end
+-- 	end
+-- end
 
 function ReadFile(FilePath)
 	-- HANDLE RELATIVE PATH OPTIONS.
